2069acd9d41bcd9095b7907b5bddb38a672789cb
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2021-07-07 07:43:42 -0500

    Using default layout for error page after removing v-app that cause extra scrolling and nested v-apps

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,31 +1,24 @@
 <template>
-  <v-app>
-    <TheAppBar @show-navigation="$refs.navigationDrawer.show()" />
-    <TheNavigationDrawer ref="navigationDrawer" />
-    <v-main>
-      <CenterPageRow>
-        <BaseCard>
-          <template #title>
-            <span class="unselectable">Error {{ error.statusCode }}</span>
-          </template>
-          <div class="unselectable text-center">
-            <p>Oh no! There was an error!</p>
-            <nuxt-link
-              to="/"
-              class="text-decoration-none"
-            >
-              Home
-            </nuxt-link>
-          </div>
-        </BaseCard>
-      </CenterPageRow>
-    </v-main>
-  </v-app>
+  <CenterPageRow>
+    <BaseCard>
+      <template #title>
+        <span class="unselectable">Error {{ error.statusCode }}</span>
+      </template>
+      <div class="unselectable text-center">
+        <p>Oh no! There was an error!</p>
+        <nuxt-link
+          to="/"
+          class="text-decoration-none"
+        >
+          Home
+        </nuxt-link>
+      </div>
+    </BaseCard>
+  </CenterPageRow>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
   props: {
     error: {
       type: Object,
